15bfb4b34f052152a08d2820ca614747a72ad0a6
Hawking <karl-ford@hotmail.com>
2021-02-16 14:39:02 +0800

    Remove all of the codes, rebuild the project

--- main.go
+++ main.go
@@ -2,29 +2,29 @@ package main
 
 import (
 	"flag"
-	"github.com/Unknwon/goconfig"
 	"log"
-	"proxy/service"
 	"strconv"
+
+	"github.com/Unknwon/goconfig"
 )
 
 var (
 	cfgPath = flag.String("config", "", "配置文件")
 
-	sport = flag.Uint("sport", 9090, "代理端口")
+	sport    = flag.Uint("sport", 9090, "代理端口")
 	protocol = flag.String("protocol", "http", "代理协议")
-	auth = flag.String("auth", "", "代理使用授权")
+	auth     = flag.String("auth", "", "代理使用授权")
 
 	adapter = flag.String("adapter", "mysql", "数据库类型")
-	host = flag.String("host", "127.0.0.1", "数据库地址")
-	port = flag.Uint("port", 3306, "数据库端口")
-	user = flag.String("user", "root", "数据库用户名")
-	pass = flag.String("pass", "", "数据库密码")
-	dbname = flag.String("dbname", "test", "数据库名称")
+	host    = flag.String("host", "127.0.0.1", "数据库地址")
+	port    = flag.Uint("port", 3306, "数据库端口")
+	user    = flag.String("user", "root", "数据库用户名")
+	pass    = flag.String("pass", "", "数据库密码")
+	dbname  = flag.String("dbname", "test", "数据库名称")
 	charset = flag.String("charset", "utf8mb4", "数据库编码")
 )
 
-func main(){
+func main() {
 	flag.Parse()
 
 	if *cfgPath != "" {
@@ -73,10 +73,5 @@ func main(){
 				}
 			}
 		}
-	}
-
-	server := service.CreateServer(sport, protocol, pass)
-	if err := server.Run(); err != nil {
-		log.Println(err)
 	}
 }
